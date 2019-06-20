201b126f3af87b4f419cd7215e642e452241036a
Jason Belgard <belgard.jason@gmail.com>
2019-06-20 16:25:01 -0500

    MVP met

---
+++ friends/src/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+import FriendList from './components/FriendList'
+import './App.css';
+import Loginpage from './components/loginpage'
+import { Route } from 'react-router-dom'
+import PrivateRoute from './components/PrivateRoute'
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Route path = '/login' component = {Loginpage} />
+        </div>
+        <div>
+          <PrivateRoute path = '/friendlist' component = {FriendList} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App
+
